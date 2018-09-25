20e2c343dac5f666a0c63361aa6d8ed6ca9af771
vijay.patel <vijay.patel@sophos.com>
2018-09-25 22:15:51 +0530

    updated structure

--- main.tf
+++ main.tf
@@ -1,28 +1,26 @@
-provider "aws" {
-  region = "${var.region}"
-}
-
-terraform {
-  required_version = "> 0.10.0"
-}
-
-locals {
-  my-queue-name = "INService"
-}
-
-module "test-sqs-dlq" {
-  source = "modules/sqs/"
-
-  queue_name                 = "${local.my-queue-name}-DLQ"
-  owner                      = "${var.owner}"
+resource "aws_sqs_queue" "default" {
+  name                       = "${var.queue_name}"
   visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
+  delay_seconds              = "${var.delay_seconds}"
+  receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
+  message_retention_seconds  = "${var.message_retention_seconds}"
+  max_message_size           = "${var.max_message_size}"
+
+  redrive_policy = "${var.dlq_arn !="" ? "{\"deadLetterTargetArn\":\"${var.dlq_arn}\",\"maxReceiveCount\":${var.max_receive_count}}" : ""}"
+
+  tags {
+    owner = "${var.owner}"
+  }
 }
 
-module "test-sqs-queue" {
-  source = "modules/sqs/"
+resource "aws_iam_policy" "sqs_iam_send_policy" {
+  name   = "${var.queue_name}-iam-send-policy"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.sqs_iam_send_policy.json}"
+}
 
-  queue_name                 = "${local.my-queue-name}"
-  owner                      = "${var.owner}"
-  dlq_arn                    = "${module.test-sqs-dlq.queue_arn}"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
+resource "aws_iam_policy" "sqs_iam_process_policy" {
+  name   = "${var.queue_name}-iam-process-policy"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.sqs_iam_process_policy.json}"
 }
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "queuename" {
-  value = "${module.test-sqs-queue.queue_name}"
-}
-
-output "queuearn" {
-  value = "${module.test-sqs-queue.queue_arn}"
-}
